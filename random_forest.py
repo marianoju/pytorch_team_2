35b7e22e527dd4f21a612ed3a391a41e8ae55f5f
Andreas <Andreas.kosch@drid.de>
2018-09-10 12:58:48 +0200

    Daten laden und Check von commit und Push

--- random_forest.py
+++ random_forest.py
@@ -1,8 +1,14 @@
-import pandas as pd
-import features
+import classifier
+from sklearn.ensemble import RandomForestRegressor
 
 
-if __name__ == '__main__':
-    data = pd.read_csv("data/housing.csv")
-    data_cleaned = features.drop_nan(data)
-    data_cleaned = pd.get_dummies(data_cleaned, drop_first=True)
+def random_forest(X_train, X_test, y_train, y_test,*,n_estimators=10, criterion='mse', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, bootstrap=True, oob_score=False, n_jobs=1, random_state=None, verbose=0, warm_start=False):
+    clf = RandomForestRegressor(n_estimators=n_estimators, criterion=criterion, max_depth=max_depth, min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf, min_weight_fraction_leaf=min_weight_fraction_leaf, max_features=max_features, max_leaf_nodes=max_leaf_nodes, min_impurity_decrease=min_impurity_decrease, min_impurity_split=min_impurity_split, bootstrap=bootstrap, oob_score=oob_score, n_jobs=n_jobs, random_state=random_state, verbose=verbose, warm_start=warm_start)
+    classifier.fit(clf, X_train, y_train)
+    rf_y_prediction = classifier.predict(clf, X_test)
+    return y_test, rf_y_prediction
+    # evaluation.print_errors(y_test, rf_y_prediction)
+
+
+
+
